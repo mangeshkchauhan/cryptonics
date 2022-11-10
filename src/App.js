4e1996f14c5c7c53a8cc1f56232413b459868eed
Mangesh <63477318+mangeshkchauhan@users.noreply.github.com>
2022-11-10 08:47:20 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Coins from "./components/Coins";
-import Exchanges from "./components/Exchanges";
-import CoinDetails from "./components/CoinDetails";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Coins from './components/Coins';
+import Exchanges from './components/Exchanges';
+import CoinDetails from './components/CoinDetails';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/coins" element={<Coins />} />
-        <Route path="/exchanges" element={<Exchanges />} />
-        <Route path="/coin/:id" element={<CoinDetails />} />
-      </Routes>
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route path='/cryptonics' element={<Home />} />
+				<Route path='/cryptonics/coins' element={<Coins />} />
+				<Route path='/cryptonics/exchanges' element={<Exchanges />} />
+				<Route path='/cryptonics/coin/:id' element={<CoinDetails />} />
+			</Routes>
 
-      <Footer />
-    </Router>
-  );
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
